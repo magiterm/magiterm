57c4522beb9493e37da6edbabe8b852cac77ed64
jeff christian <jchri@MacBook-Pro-2.loca>
2016-11-17 12:27:23 -0800

    updated session life to one day

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+app.use(session({ 
+  secret: 'keyboard cat', 
+  cookie: { maxAge: 86400000 }
+}));
+
 app.use(passport.initialize());
 
 app.use(express.static(path.join(__dirname, 'public')));
