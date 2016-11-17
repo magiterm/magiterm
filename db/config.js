5f5eeccfe6ebb827c7e99d4169eff32059142a99
Lucas Becker <Lucas.D.Becker@gmail.com>
2016-11-17 12:27:04 -0800

    Merge goodInvites

--- db/config.js
+++ db/config.js
@@ -1,10 +1,6 @@
 const Sequelize = require('sequelize');
 
-const LOCAL_DB_URL = 'mysql://root:root@127.0.0.1:3306/demo_schema';
-
-const db = new Sequelize('demo_schema', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
+const LOCAL_DB_URL = 'mysql://root:root@127.0.0.1:3306/picodb';
 
 const db = new Sequelize(process.env.JAWSDB_URL || LOCAL_DB_URL, {
   pool: {
